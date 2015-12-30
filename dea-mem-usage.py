0299ad2005e5d4e7d639f90f6bf81a4d17234a8d
Miki Wiik <miki.wiik@service-flow.com>
2015-12-30 12:44:04 +0200

    Whitespace

--- dea-mem-usage.py
+++ dea-mem-usage.py
@@ -3,48 +3,49 @@ import subprocess
 import re
 
 class Node:
-        def __init__(self, node_string):
-                node_info = node_string.split()
-                self.ip = node_info[0]
-                self.role = node_info[2]
+    def __init__(self, node_string):
+        node_info = node_string.split()
+        self.ip = node_info[0]
+        self.role = node_info[2]
 
-        def __repr__(self):
-                return '{} {}'.format(self.ip, self.role) 
+    def __repr__(self):
+        return '{} {}'.format(self.ip, self.role)
 
-        def isdea(self):
-                return "dea" == self.role
+    def isdea(self):
+        return "dea" == self.role
 
 class DockerInstance:
-        def __init__(self, ps_string):
-                # The beginning of the line is the alphanumeric Container Id
-                id_search = re.search("^\w+", ps_string, re.UNICODE)
-                if id_search:
-                        self.id = id_search.group()
-                else:
-                        raise ValueError("Unable to parse Container Id from\n{}".format(ps_string))
+    def __init__(self, ps_string):
+        # The beginning of the line is the alphanumeric Container Id
+        id_search = re.search("^\w+", ps_string, re.UNICODE)
+        if id_search:
+            self.id = id_search.group()
+        else:
+            raise ValueError("Unable to parse Container Id from\n{}".format(ps_string))
 
-                # The end of the line is the Container name
-                name_search = re.search("([\w-])+\s*$", ps_string, re.UNICODE)
-                if name_search:
-                        self.name = name_search.group()
-                else:
-                        raise ValueError("Unable to parse Container name from\n{}".format(ps_string))
-        
-        def getId(self):
-                return self.id
+        # The end of the line is the Container name
+        name_search = re.search("([\w-])+\s*$", ps_string, re.UNICODE)
+        if name_search:
+            self.name = name_search.group()
+        else:
+            raise ValueError("Unable to parse Container name from\n{}".format(ps_string))
 
-        def __repr__(self):
-                return "Container id: '{}', name: '{}'".format(self.id, self.name)
+    def getId(self):
+        return self.id
 
-#if __name__ == "__main__":
+    def __repr__(self):
+        return "Container id: '{}', name: '{}'".format(self.id, self.name)
+
+# if __name__ == "__main__":
 kato_nodes = subprocess.check_output(["kato", "node", "list"])
-#print(kato_nodes)
+# print(kato_nodes)
 for node in map(Node, kato_nodes.splitlines()):
-        if node.isdea():
-                print("Node:" + str(node))
-                docker_instances = subprocess.check_output(["ssh", node.ip, "docker ps"])
-                for instance in map(DockerInstance, docker_instances.splitlines()[1:]):
-                        print(instance)
-                        #print(subprocess.check_output(["ssh", node.getIp(), "docker inspect", instance.getId()]))
-                        import sys
-                        sys.exit()
+    if node.isdea():
+        print("Node:" + str(node))
+        docker_instances = subprocess.check_output(["ssh", node.ip, "docker ps"])
+        for instance in map(DockerInstance, docker_instances.splitlines()[1:]):
+            print(instance)
+            # print(subprocess.check_output(["ssh", node.getIp(), "docker inspect", instance.getId()]))
+            import sys
+
+            sys.exit()
